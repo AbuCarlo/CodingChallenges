3541d1dd191b3f7a63e5fa844334f1c00fe8cb6b
Tony Nassar <anthonyabunassar@gmail.com>
2024-11-05 13:00:25 -0500

    Added PBTs...which fail

--- ccwc/go.mod
+++ ccwc/go.mod
@@ -5,6 +5,7 @@ go 1.22.1
 require (
 	github.com/jessevdk/go-flags v1.6.1
 	golang.org/x/exp v0.0.0-20241009180824-f66d83c29e7c
+	pgregory.net/rapid v1.1.0
 )
 
 require golang.org/x/sys v0.26.0 // indirect
--- ccwc/wc_test.go
+++ ccwc/wc_test.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"os/exec"
 	"strings"
 	"testing"
 
 	"anthonyabunassar.com/coding-challenges/ccwc/options"
 	"github.com/jessevdk/go-flags"
+    "pgregory.net/rapid"
 )
 
 func parseOptions(line []string) (options.WcOptions, []string, error) {
@@ -17,33 +17,48 @@ func parseOptions(line []string) (options.WcOptions, []string, error) {
     return options, files, err
 }
 
+var validArguments = []string{
+    "-c", "-m", "-l", "-L", "-w",
+}
+
+var validTotalArgValues = []string{
+    "always", "auto", "only", "never",
+}
+
+func generateValidArgs(t *rapid.T) []string {
+	totalsGenerator := rapid.Custom(func(t *rapid.T) string {
+		totalValue := rapid.SliceOfN(rapid.SampledFrom(validTotalArgValues), 1, 1).Draw(t, "tv")
+		return "--total=" + totalValue[0]
+	})
+	totalArg := rapid.SliceOfN(totalsGenerator, 0, 1).Draw(t, "t")
+	args := rapid.SliceOfDistinct(rapid.SampledFrom(validArguments), func(s string) string { return s }).Draw(t, "o")
+	args = append(args, totalArg...)
+	return args
+}
+
 func TestConsoleOutput(t *testing.T) {
-	testCases := [][]string{
-		{"./test-files/hello.txt"},
+	inputs := []string{
+		"./test-files/hello.txt",
 	}
-	for i, testCase := range testCases {
-		t.Run(fmt.Sprintf("Test %d", i), func(t *testing.T) {
-			out, err := exec.Command("wc", testCase...).Output()
-			if err != nil {
-                // Something is wrong with the test, or wc is not on the path.
-				stderr := err.(*exec.ExitError).Stderr
-				t.Fatal(string(stderr))
-			}
 
-            // Command-line parsing has been externalized from main().
-            options, files, err := parseOptions(testCase)
-            if err != nil {
-                // Fix the test case. We're testing only valid options.
-                t.Fatal(err)
-            }
+    rapid.Check(t, func(t *rapid.T) {
+        args := generateValidArgs(t)
+        args = append(args, inputs...)
+        out, err := exec.Command("wc", args...).Output()
+        if err != nil {
+            // Something is wrong with the test, or wc is not on $PATH.
+            stderr := err.(*exec.ExitError).Stderr
+            t.Fatal(string(stderr))
+        }
+        expected := string(out)
 
-            builder := strings.Builder{}
-            ReadFiles(files, options, &builder)
-            actual := builder.String()
+        wcOptions, files, _ := parseOptions(args)
+        builder := strings.Builder{}
+        ReadFiles(files, wcOptions, &builder)
+        actual := builder.String()
 
-            if actual != string(out) {
-                t.Errorf("%v failed.\nExpected:\n%s\nGot:\n%s\n", testCase, string(out), actual)
-            }
-		})
-	}
+        if actual != expected {
+            t.Errorf("%v failed.\nExpected:\n%s\nGot:\n%s\n", args, string(out), actual)
+        }
+	})
 }
